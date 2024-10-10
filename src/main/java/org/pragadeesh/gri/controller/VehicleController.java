3f97cb54d6962b7ee70778c12a864377b1e89d54
pragadeesh <pragadeesh.online@gmail.com>
2024-10-10 17:09:15 +0530

    added vehicle controller

--- src/main/java/org/pragadeesh/gri/controller/VehicleController.java
+++ src/main/java/org/pragadeesh/gri/controller/VehicleController.java
@@ -1,8 +1,13 @@
 package org.pragadeesh.gri.controller;
 
+import org.pragadeesh.gri.entity.Vehicle;
 import org.pragadeesh.gri.service.VehicleService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/manager")
@@ -12,5 +17,16 @@ public class VehicleController {
 
     public VehicleController(VehicleService vehicleService) {
         this.vehicleService = vehicleService;
+    }
+
+    @PostMapping("/vehicle/project/{projectId}")
+    public ResponseEntity<Vehicle> uploadVehiclePhotos(@PathVariable UUID projectId,
+                                                       @RequestBody List<MultipartFile> file) throws Exception {
+        return ResponseEntity.ok(vehicleService.uploadVehiclePhotos(projectId, file));
+    }
+
+    @GetMapping("/vehicle/project/{projectId}")
+    public List<Vehicle> getAllVehicleByProject(@PathVariable UUID projectId) {
+        return vehicleService.getAllVehiclesForProject(projectId);
     }
 }
